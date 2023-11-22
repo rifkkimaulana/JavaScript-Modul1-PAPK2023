762f1800c9055550687ed88c0446f5b924e246c9
rifkkimaulana <rifkkimaulana@gmail.com>
2023-11-22 11:29:38 +0700

    include all assets app calculator

--- Pertemuan 9 - Service Worker/Kalkulator/service-worker.js
+++ Pertemuan 9 - Service Worker/Kalkulator/service-worker.js
@@ -3,23 +3,34 @@ const toCache = [
   "/",
   "manifest.json",
   "assets/js/register.js",
-  "assets/img/icon.png",
+  "assets/css/bootstrap.css",
+  "assets/css/bootstrap.css.map",
+  "assets/css/styles.css",
+  "assets/images/calculator.png",
+  "assets/images/calculator-192.png",
+  "assets/js/bootstrap.js",
+  "assets/js/bootstrap.js.map",
+  "assets/js/jquery-3.3.1.slim.min.js",
+  "assets/js/script.js",
+  "index.html",
 ];
 
-let deferredPrompt;
-
 self.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
   e.preventDefault();
+  // Stash the event so it can be triggered later.
   deferredPrompt = e;
-
   showInstallPromotion();
 });
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(toCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(toCache);
+      })
+      .then(self.skipWaiting())
   );
 });
 
@@ -35,15 +46,18 @@ self.addEventListener("fetch", function (event) {
 
 self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Hapus cache lama", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches
+      .keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log("[ServiceWorker] Hapus cache lama", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
